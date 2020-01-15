023776ef472aa976dad48c793a55d269efbcc0b7
田中 真樹 <lauramt@MacBook-Pro.local>
2020-01-15 16:12:14 +0900

    done

--- question1.py
+++ question1.py
@@ -1,7 +1,8 @@
 import numpy as np
 #Do not import any other libraries
 """
-Write a function that takes a number n as input and returns a 2 dimensional array of size nxn where the returned array has the following form.
+Write a function that takes a number n as input and returns a 2 dimensional array of size nxn
+where the returned array has the following form.
 
 f(1) returns array([[1]])
 f(2) returns array([[2,1],
@@ -14,11 +15,28 @@ f(4) returns array([[4,3,2,1],
                     [2,2,2,1],
                     [1,1,1,1]])
 
-And so on. Hint: it is possible to add to a subset of an array. For example, say arr = np.zeros(4). Then if you run arr[:2] = arr[:2] + np.array([1,2])
-and print arr, array([1,2,0,0]) would be outputed. Make sure you are returning a numpy array and not a list of lists.
+And so on.
+Hint:
+it is possible to add to a subset of an array.
+For example, say arr = np.zeros(4). Then if you run arr[:2] = arr[:2] + np.array([1,2])
+and print arr, array([1,2,0,0]) would be outputed.
+Make sure you are returning a numpy array and not a list of lists.
 """
 
 def f(n):
     ##########YOUR CODE HERE##########
-    pass
+    arr = np.zeros((n,n))
+    b = np.zeros((n,n))
+
+#initialize
+    arr[0] = np.arange(n,0,-1)
+
+    for i in range(n):
+        b[i,:i]=1
+
+        if i>0:
+            arr[i] = arr[i-1] - b[i]
+
+    return arr
+
     ###########END CODE###############
